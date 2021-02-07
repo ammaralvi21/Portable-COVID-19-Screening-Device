11110c72ed3ae32d2611d087efc8be7d8ef5677b
Ammar Alvi <ammaralvi21@gmail.com>
2021-02-07 01:14:07 -0600

    Configured as I2C slave device

--- STM32_IR_UI/User_Interface/Core/Src/main.c
+++ STM32_IR_UI/User_Interface/Core/Src/main.c
@@ -29,6 +29,15 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef enum 
+{ 
+	IR_UI,
+	TEMP,
+	SPO2,
+	SANITIZE,
+	IDLE
+	
+} SenseState;
 
 /* USER CODE END PTD */
 
@@ -38,14 +47,21 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+#define I2C_SET_TEMP 0x01
+#define I2C_SET_SPO2 0x02
+#define I2C_SET_UI 0x03
+#define I2C_SET_SANITIZER 0x04
+#define I2C_SET_IDLE 0x05
 
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 
-UART_HandleTypeDef huart2;
+I2C_HandleTypeDef hi2c2;
 
+UART_HandleTypeDef huart2;
+SenseState g_State = IR_UI;
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -55,6 +71,8 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_NVIC_Init(void);
 /* USER CODE BEGIN PFP */
 void ADC_CH6_Enable(void);
 void ADC_CH5_Enable(void);
@@ -62,9 +80,13 @@ void ADC_CH5_Enable(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define I2C_TX_BUFF_SIZE 4
+volatile uint16_t IR_Right = 0;
+volatile uint16_t IR_Left = 0;
+volatile uint8_t g_i2c_rx_buff = 0;
+volatile uint8_t g_i2c_tx_buff = 0;
 
-	volatile uint16_t IR_Right = 0;
-	volatile uint16_t IR_Left = 0;
+volatile uint8_t g_IR_UI_status = 0;
 /* USER CODE END 0 */
 
 /**
@@ -97,8 +119,14 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_ADC1_Init();
+  MX_I2C2_Init();
+
+  /* Initialize interrupts */
+  MX_NVIC_Init();
   /* USER CODE BEGIN 2 */
-	
+	g_i2c_rx_buff = 0;
+//	HAL_I2C_Slave_Receive_IT(&hi2c2, (uint8_t *) &g_i2c_rx_buff, 1);
+	HAL_I2C_EnableListen_IT(&hi2c2);
 	
 
   /* USER CODE END 2 */
@@ -114,27 +142,58 @@ int main(void)
 	//HAL_ADCEx_Calibration_Start(&hadc1);
   while (1)
   {
-	  ADC_CH6_Enable();
-		HAL_ADC_Start(&hadc1);
-		HAL_ADC_PollForConversion(&hadc1, 1000);
-		IR_Right = HAL_ADC_GetValue(&hadc1);
-		HAL_ADC_Stop(&hadc1);
-
-		ADC_CH5_Enable();
-		HAL_ADC_Start(&hadc1);
-		HAL_ADC_PollForConversion(&hadc1, 1000);
-		IR_Left = HAL_ADC_GetValue(&hadc1);
-		HAL_ADC_Stop(&hadc1);
 		
-		sprintf(Left_IR, "%d", IR_Left);
-		sprintf(Right_IR, "%d", IR_Right);
-		HAL_UART_Transmit(&huart2,(uint8_t *)Left_IR,strlen(Left_IR), 1000);
-		HAL_UART_Transmit(&huart2,(uint8_t *)"  ",2, 1000);
-		HAL_UART_Transmit(&huart2,(uint8_t *)Right_IR,strlen(Right_IR), 1000);
-			HAL_UART_Transmit(&huart2,(uint8_t *)"\r\n",2, 1000);
-		memset(Left_IR, '\0', sizeof(Left_IR));
-		memset(Right_IR, '\0', sizeof(Right_IR));
-			HAL_Delay(100);
+		switch (g_State)
+		{	
+			case IR_UI:
+				ADC_CH6_Enable();
+				HAL_ADC_Start(&hadc1);
+				HAL_ADC_PollForConversion(&hadc1, 1000);
+				IR_Right = HAL_ADC_GetValue(&hadc1);
+				HAL_ADC_Stop(&hadc1);
+
+				ADC_CH5_Enable();
+				HAL_ADC_Start(&hadc1);
+				HAL_ADC_PollForConversion(&hadc1, 1000);
+				IR_Left = HAL_ADC_GetValue(&hadc1);
+				HAL_ADC_Stop(&hadc1);
+
+				if ((IR_Right > 800) && (IR_Left < 800))
+				{
+					g_IR_UI_status = 0x1;
+				}
+				else if((IR_Right < 800) && (IR_Left > 800))
+				{
+					g_IR_UI_status = 0x4;
+				}
+				else if ((IR_Right > 800) && (IR_Left > 800))
+				{
+					g_IR_UI_status = 0x9;
+				}
+				else
+				{
+					g_IR_UI_status = 0xF;
+				}
+				g_i2c_tx_buff = g_IR_UI_status;
+				sprintf(Left_IR, "%d", IR_Left);
+				sprintf(Right_IR, "%d", IR_Right);
+				HAL_UART_Transmit(&huart2,(uint8_t *)Left_IR,strlen(Left_IR), 1000);
+				HAL_UART_Transmit(&huart2,(uint8_t *)"  ",2, 1000);
+				HAL_UART_Transmit(&huart2,(uint8_t *)Right_IR,strlen(Right_IR), 1000);
+				HAL_UART_Transmit(&huart2,(uint8_t *)"\r\n",2, 1000);
+				memset(Left_IR, '\0', sizeof(Left_IR));
+				memset(Right_IR, '\0', sizeof(Right_IR));
+				HAL_Delay(100);
+				break;
+			case TEMP:
+				break;
+			case SPO2:
+				break;
+			case SANITIZE:
+				break;
+			case IDLE:
+				break;
+			}
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -187,6 +246,20 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief NVIC Configuration.
+  * @retval None
+  */
+static void MX_NVIC_Init(void)
+{
+  /* I2C2_EV_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(I2C2_EV_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(I2C2_EV_IRQn);
+  /* I2C2_ER_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(I2C2_ER_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(I2C2_ER_IRQn);
+}
+
+/**
   * @brief ADC1 Initialization Function
   * @param None
   * @retval None
@@ -198,7 +271,7 @@ static void MX_ADC1_Init(void)
 
   /* USER CODE END ADC1_Init 0 */
 
-  //ADC_ChannelConfTypeDef sConfig = {0};
+//  ADC_ChannelConfTypeDef sConfig = {0};
 
   /* USER CODE BEGIN ADC1_Init 1 */
 
@@ -216,14 +289,62 @@ static void MX_ADC1_Init(void)
   {
     Error_Handler();
   }
-
-
+  /** Configure Regular Channel
+  */
+//  sConfig.Channel = ADC_CHANNEL_5;
+//  sConfig.Rank = ADC_REGULAR_RANK_1;
+//  sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;
+//  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
+//  /** Configure Regular Channel
+//  */
+//  sConfig.Channel = ADC_CHANNEL_6;
+//  sConfig.Rank = ADC_REGULAR_RANK_2;
+//  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+//  {
+//    Error_Handler();
+//  }
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
 
 }
 
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 128;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
 
 /**
   * @brief USART2 Initialization Function
@@ -271,6 +392,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, LD4_Pin|LD3_Pin, GPIO_PIN_RESET);
@@ -285,6 +407,69 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_I2C_AddrCallback (I2C_HandleTypeDef * hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode)
+{
+	UNUSED(AddrMatchCode);
+	
+	switch (TransferDirection)
+	{
+		case I2C_DIRECTION_TRANSMIT:
+			if (HAL_I2C_Slave_Seq_Receive_IT(&hi2c2, (uint8_t *) &g_i2c_rx_buff, 1, I2C_NEXT_FRAME) != HAL_OK) 
+			{
+				 Error_Handler();
+			}
+
+			break;
+		case I2C_DIRECTION_RECEIVE:
+			if (HAL_I2C_Slave_Seq_Transmit_IT(&hi2c2, (uint8_t *) &g_i2c_tx_buff, 1, I2C_LAST_FRAME) != HAL_OK) 
+			{
+				 Error_Handler();
+			}
+			break;
+	}
+
+}
+
+
+void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	//Send completion callback function 
+	g_i2c_tx_buff = 0;
+
+}
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	//Receive completion callback function
+	switch (g_i2c_rx_buff)
+	{
+		case I2C_SET_SPO2: 
+			g_State = SPO2;
+			break;
+		case I2C_SET_TEMP :
+			g_State = TEMP;
+			break;
+		case I2C_SET_UI :
+			g_State = IR_UI;
+			break;
+		case I2C_SET_SANITIZER :
+			g_State = SANITIZE;
+			break;
+		case I2C_SET_IDLE :
+			g_State = IDLE;
+			break;
+
+	}
+	
+	g_i2c_rx_buff = 0;
+	
+	
+}
+
+void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c)
+{ 
+  HAL_I2C_EnableListen_IT(hi2c); // Restart
+}
+
 void ADC_CH5_Enable(void)
 {
   /** Configure Regular Channel
